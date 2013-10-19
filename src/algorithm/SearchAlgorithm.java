1e48479c95ceb25f36f00febe4baac68f24aeca8
mina.workspace@gmail.com <mina.workspace@gmail.com@023ba5da-7fea-1741-fbe1-9d6d785f80c4>
2013-10-19 19:06:59 +0000

    Fixed (added problem in strategy) SearchAlgorihtm

--- src/algorithm/SearchAlgorithm.java
+++ src/algorithm/SearchAlgorithm.java
@@ -9,53 +9,68 @@ public class SearchAlgorithm {
 
 	ArrayList<SearchTreeNode> nodes;
 
-	public SearchTreeNode search(SearchProblem problem, int strategy){
+	public SearchTreeNode search(SearchProblem problem, int strategy) {
 		nodes = new ArrayList<SearchTreeNode>();
-		nodes.add(new SearchTreeNode(problem.getInitialState(),null,null,0,0));
-		while(!nodes.isEmpty()){
+		nodes.add(new SearchTreeNode(problem.getInitialState(), null, null, 0,
+				0));
+		while (!nodes.isEmpty()) {
 			SearchTreeNode node = nodes.get(0);
-			if(problem.goalTest(node.getState())){
+			if (problem.goalTest(node.getState())) {
 				return node;
-			}else{
-				switch(strategy){
-				case 0:BFS(node);break;
-				case 1:DFS(node);break;
-				case 2:IDS(node);break;
-				case 3:GRS0(node);break;
-				case 4:GRS1(node);break;
-				case 5:AS0(node);break;
-				case 6:AS1(node);break;	
+			} else {
+				switch (strategy) {
+				case 0:
+					BFS(node, problem);
+					break;
+				case 1:
+					DFS(node, problem);
+					break;
+				case 2:
+					IDS(node, problem);
+					break;
+				case 3:
+					GRS0(node, problem);
+					break;
+				case 4:
+					GRS1(node, problem);
+					break;
+				case 5:
+					AS0(node, problem);
+					break;
+				case 6:
+					AS1(node, problem);
+					break;
 				}
 			}
 		}
 		return null;
 	}
 
-	public void BFS(SearchTreeNode node) {
+	public void BFS(SearchTreeNode node, SearchProblem problem) {
 
 	}
 
-	public void DFS(SearchTreeNode node) {
+	public void DFS(SearchTreeNode node, SearchProblem problem) {
 
 	}
 
-	public void IDS(SearchTreeNode node) {
+	public void IDS(SearchTreeNode node, SearchProblem problem) {
 
 	}
 
-	public void GRS0(SearchTreeNode node) {
+	public void GRS0(SearchTreeNode node, SearchProblem problem) {
 
 	}
 
-	public void GRS1(SearchTreeNode node) {
+	public void GRS1(SearchTreeNode node, SearchProblem problem) {
 
 	}
 
-	public void AS0(SearchTreeNode node) {
+	public void AS0(SearchTreeNode node, SearchProblem problem) {
 
 	}
 
-	public void AS1(SearchTreeNode node) {
+	public void AS1(SearchTreeNode node, SearchProblem problem) {
 
 	}
 
